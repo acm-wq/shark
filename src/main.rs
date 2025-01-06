2c998d10fbb111e3aa7a4e46af17d922e4aa85f9
acm-wq <camisglhdadae@gmail.com>
2025-01-06 16:22:25 +0700

    feat: add word saving to json

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
 use gtk::prelude::*;
-use gtk::{glib, Application, ApplicationWindow};
+use gtk::{glib, Application, ApplicationWindow, Entry, Button, Box};
+use serde_json::json;
+use std::fs::File;
+use std::rc::Rc;
 
 fn main() -> glib::ExitCode {
     let app = Application::builder()
@@ -7,7 +10,6 @@ fn main() -> glib::ExitCode {
         .build();
 
     app.connect_activate(|app| {
-        // We create the main window.
         let window = ApplicationWindow::builder()
             .application(app)
             .default_width(320)
@@ -15,7 +17,29 @@ fn main() -> glib::ExitCode {
             .title("Hello, World!")
             .build();
 
-        // Show the window.
+        let vbox = Box::new(gtk::Orientation::Vertical, 5);
+        
+        let entry = Rc::new(Entry::new());
+        entry.set_placeholder_text(Some("Enter words with a space"));
+
+        let button = Button::with_label("Save in JSON");
+        
+        let entry_clone = Rc::clone(&entry);
+        button.connect_clicked(move |_| {
+            let text = entry_clone.text().to_string();
+            let words: Vec<&str> = text.split_whitespace().collect();
+
+            let json_data = json!({ "words": words });
+
+            let file = File::create("src/words.json").expect("Failed to write to file");
+            serde_json::to_writer(file, &json_data).expect("Failed to write to file");
+        });
+
+        vbox.append(&*entry);
+        vbox.append(&button);
+        
+        window.set_child(Some(&vbox));
+
         window.present();
     });
 
