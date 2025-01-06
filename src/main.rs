ba2bbe0c0a9a0a29108d05722c7436337932eecf
acm-wq <camisglhdadae@gmail.com>
2025-01-06 21:34:18 +0700

    fix: word output on reboot

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,34 @@
 use gtk::prelude::*;
 use gtk::{glib, Application, ApplicationWindow, Entry, Button, Box, Label};
 use serde_json::json;
+use serde_json::Value;
 use std::fs::File;
+use std::io::Read;
 use std::rc::Rc;
+
+fn load_words_from_file() -> Vec<String> {
+    let mut file = match File::open("src/words.json") {
+        Ok(file) => file,
+        Err(_) => return Vec::new(),
+    };
+
+    let mut contents = String::new();
+    if file.read_to_string(&mut contents).is_err() {
+        return Vec::new();
+    }
+
+    let json_data: Value = match serde_json::from_str(&contents) {
+        Ok(data) => data,
+        Err(_) => return Vec::new(),
+    };
+
+    json_data["words"]
+        .as_array()
+        .unwrap_or(&vec![])
+        .iter()
+        .filter_map(|v| v.as_str().map(String::from))
+        .collect()
+}
 
 fn main() -> glib::ExitCode {
     let app = Application::builder()
@@ -26,6 +52,11 @@ fn main() -> glib::ExitCode {
         
         let label = Label::new(Some("List of words:"));
         
+        let words = load_words_from_file();
+        if !words.is_empty() {
+            label.set_text(&format!("List of words: {}", words.join(", ")));
+        }
+
         let entry_clone = Rc::clone(&entry);
         let label_clone = label.clone();
         button.connect_clicked(move |_| {
